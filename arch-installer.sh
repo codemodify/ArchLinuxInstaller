5545b82b83e17f18c284635fab83fbed2c7a5a2d
Nicolae Carabut <nicolae.carabut@gmail.com>
2016-06-27 14:16:50 -0700

    v1

--- arch-installer.sh
+++ arch-installer.sh
@@ -7,15 +7,15 @@ set RootDevice=$3 # Required
 function AreRequirementsSatisfied() {
     local satisfied=true
 
-    if [! -b $BootDevice] ; then
+    if ![-b $BootDevice] ; then
         satisfied=false
         echo "$BootDevice does not exist to be used as 'boot', run cfdisk and create it."
     fi
-    if [$SwapDevice !-eq "none"] && [! -b $SwapDevice] ; then
+    if [$SwapDevice -ne "none"] && [! -b $SwapDevice] ; then
         satisfied=false
         echo "$SwapDevice does not exist to be used as 'swap', run cfdisk and create it."
     fi
-    if [! -b $RootDevice] ; then
+    if ![ -b $RootDevice] ; then
         satisfied=false
         echo "$RootDevice does not exist to be used as 'root', run cfdisk and create it."
     fi
