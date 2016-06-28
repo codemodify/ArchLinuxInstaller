2d9224b4e7130fb6e0073a77208446148629c144
Nicolae Carabut <nicolae.carabut@gmail.com>
2016-06-28 09:04:16 -0700

    v1

--- ArchInstaller.sh
+++ ArchInstaller.sh
@@ -6,7 +6,7 @@ cd /temp-ArchInstaller
 curl -o node.gz https://nodejs.org/dist/v6.2.2/node-v6.2.2-linux-x64.tar.gz
 tar -xzvf node.gz
 mv node-*-linux-* nodedist
-PATH=$PATH;/temp-ArchInstaller/nodedist/bin
+PATH=$PATH:/temp-ArchInstaller/nodedist/bin
 
 # NodeJS is ready, prep the Installer's dependencies
 curl -o package.json https://raw.githubusercontent.com/nic0lae/ArchLinuxInstaller/master/package.json
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ###### Automated Installer for [Arch Linux](https://www.archlinux.org)
 ###### License: [TFL](https://github.com/nic0lae/TrueFreeLicense)
+###### Install
+curl -o ArchIntaller.sh https://raw.githubusercontent.com/nic0lae/ArchLinuxInstaller/master/ArchInstaller.sh && chmod +x ./ArchIntaller.sh && ./ArchIntaller.sh 
 
 ###### Assumes This Layout
 - /boot
