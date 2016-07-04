e94fd7f801263ecebf7bed9c5501158e4430f76a
Nicolae Carabut <nicolae.carabut@gmail.com>
2016-07-04 01:38:19 -0400

    v1

--- ArchInstaller.sh
+++ ArchInstaller.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 
-# Prep the environment for NodeJS
-mkdir /temp-ArchInstaller
-cd /temp-ArchInstaller
-curl -o node.gz https://nodejs.org/dist/v6.2.2/node-v6.2.2-linux-x64.tar.gz
-tar -xf node.gz
-mv node-*-linux-* nodedist
+# Prep the environment
+curl -o Installer.sh https://raw.githubusercontent.com/nic0lae/LinuxInstaller/master/Installer/Installer.sh
+chmod +x ./Installer.sh
+./Installer.sh
 PATH=$PATH:/temp-ArchInstaller/nodedist/bin
-
-# NodeJS is ready, Installer's dependencies
-curl -o package.json https://raw.githubusercontent.com/nic0lae/LinuxInstaller/master/Installer/package.json
-npm install
-
 
 # Run the installer
 curl -o LinuxInstaller.Arch.ts \
         https://raw.githubusercontent.com/nic0lae/LinuxInstaller/master/Installer/LinuxInstaller.Arch.ts
 
 npm run tsc
-node LinuxInstaller.Arch.js
+#node LinuxInstaller.Arch.js
